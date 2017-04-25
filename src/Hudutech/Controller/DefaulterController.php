98819c0b57dbad92a8812e6f99dcb6963387890c
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 18:39:57 +0300

    added implements methods in defaulter ctrl

--- src/Hudutech/Controller/DefaulterController.php
+++ src/Hudutech/Controller/DefaulterController.php
@@ -9,7 +9,39 @@
 namespace Hudutech\Controller;
 
 
-class DefaulterController
+use Hudutech\AppInterface\DefaulterInterface;
+use Hudutech\Entity\Defaulter;
+
+class DefaulterController implements DefaulterInterface
 {
+    public function create(Defaulter $defaulter)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(Defaulter $defaulter, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public static function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function destroy()
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public static function getDefaulterObject($id)
+    {
+        // TODO: Implement getDefaulterObject() method.
+    }
+
+    public static function all()
+    {
+        // TODO: Implement all() method.
+    }
 
 }
