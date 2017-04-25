77cc62b489a137303baf622c586adb8259529049
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 13:18:58 +0300

    added employee interface

--- src/Hudutech/AppInterface/EmployeeInterface.php
+++ src/Hudutech/AppInterface/EmployeeInterface.php
@@ -9,7 +9,14 @@
 namespace Hudutech\AppInterface;
 
 
+use Hudutech\Entity\Employee;
+
 interface EmployeeInterface
 {
-
+    public function create(Employee $employee);
+    public function update(Employee $employee, $id);
+    public static function delete($id);
+    public static function destroy();
+    public static function getEmployeeObject($id);
+    public static function all();
 }
