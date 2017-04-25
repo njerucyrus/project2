6d0f04af944c47ba4cae9ae711352e3a0c729aef
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 13:16:01 +0300

    added client interface

--- src/Hudutech/AppInterface/ClientInterface.php
+++ src/Hudutech/AppInterface/ClientInterface.php
@@ -9,7 +9,15 @@
 namespace Hudutech\AppInterface;
 
 
+use Hudutech\Entity\Client;
+
 interface ClientInterface
 {
+  public function create(Client $client);
+  public function update(Client $client, $id);
+  public static function delete($id);
+  public static function destroy();
+  public static function getClientObject($id);
+  public static function all();
 
 }
