b76ac1c56f5b383c9ec6304ca6a81269ecff771b
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 12:50:04 +0300

    added defulter entity

--- src/Hudutech/Entity/Defaulter.php
+++ src/Hudutech/Entity/Defaulter.php
@@ -11,5 +11,85 @@ namespace Hudutech\Entity;
 
 class Defaulter
 {
+    /**
+     * @var integer
+     */
+    private $id;
+    /**
+     * @var integer
+     */
+    private $clientId;
+    /**
+     * @var integer
+     */
+    private $groupId;
+    /**
+     * @var float
+     */
+    private $amountDefulted;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @param int $clientId
+     */
+    public function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupId()
+    {
+        return $this->groupId;
+    }
+
+    /**
+     * @param int $groupId
+     */
+    public function setGroupId($groupId)
+    {
+        $this->groupId = $groupId;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmountDefulted()
+    {
+        return $this->amountDefulted;
+    }
+
+    /**
+     * @param float $amountDefulted
+     */
+    public function setAmountDefulted($amountDefulted)
+    {
+        $this->amountDefulted = $amountDefulted;
+    }
 
 }
