9cd0b449a896044433ddcf8f981c314afb0b4ecf
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 20:22:19 +0300

    completed create and update methods in defaulter ctrl

--- src/Hudutech/Controller/DefaulterController.php
+++ src/Hudutech/Controller/DefaulterController.php
@@ -10,18 +10,70 @@ namespace Hudutech\Controller;
 
 
 use Hudutech\AppInterface\DefaulterInterface;
+use Hudutech\DBManager\DB;
 use Hudutech\Entity\Defaulter;
 
 class DefaulterController implements DefaulterInterface
 {
     public function create(Defaulter $defaulter)
     {
-        // TODO: Implement create() method.
+        $db = new DB();
+        $conn = $db->connect();
+        $clientId = $defaulter->getClientId();
+        $groupId = $defaulter->getGroupId();
+        $amountDefaulted = $defaulter->getAmountDefulted();
+
+        try{
+            $stmt = $conn->prepare("INSERT INTO defaulters(
+                                                            client_id,
+                                                            group_id,
+                                                            amount_defaulted
+                                                          ) 
+                                                    VALUES
+                                                     (
+                                                        :client_id,
+                                                        :group_id,
+                                                        :amount_defaulted
+                                                     )");
+            $stmt->bindParam(":client_id", $clientId);
+            $stmt->bindParam(":group_id", $groupId);
+            $stmt->bindParam(":amount_defaulted", $amountDefaulted);
+
+            return $stmt->execute() ? true : false;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
+
     }
 
     public function update(Defaulter $defaulter, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn = $db->connect();
+        $clientId = $defaulter->getClientId();
+        $groupId = $defaulter->getGroupId();
+        $amountDefaulted = $defaulter->getAmountDefulted();
+
+        try{
+            $stmt = $conn->prepare("UPDATE defaulters SET  
+                                                            client_id:=client_id,
+                                                            group_id:=group_id,
+                                                            amount_defaulted:=amount_defaulted
+                                                      WHERE id=:id
+                                  ");
+
+            $stmt->bindParam(":client_id", $clientId);
+            $stmt->bindParam(":group_id", $groupId);
+            $stmt->bindParam(":amount_defaulted", $amountDefaulted);
+
+            return $stmt->execute() ? true : false;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function delete($id)
