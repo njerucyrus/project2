afe3ce2f7c267041cf7b41eb5fb163eefb535b11
njerucyrus <njerucyrus5@hotmail.com>
2017-04-25 14:52:56 +0300

    completed update Employee

--- src/Hudutech/Controller/EmployeeController.php
+++ src/Hudutech/Controller/EmployeeController.php
@@ -102,7 +102,6 @@ class EmployeeController implements EmployeeInterface
             $stmt->bindParam(":nok_name",$nokName);
             $stmt->bindParam(":nok_relationship",$nokRelationship);
             $stmt->bindParam(":nok_contact",$nokContact);
-
             return $stmt->execute() ? true : false;
 
         } catch (\PDOException $exception) {
@@ -115,7 +114,79 @@ class EmployeeController implements EmployeeInterface
 
     public function update(Employee $employee, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        $pfNo = $employee->getPfNo();
+        $fullName = $employee->getFullName();
+        $jobTitle = $employee->getJobTitle();
+        $idNo = $employee->getIdNo();
+        $nssfNo = $employee->getNssfNo();
+        $nhifNo = $employee->getNhifNo();
+        $remuneration = $employee->getRemuneration();
+        $jobDescription = $employee->getJobDescription();
+        $qualification = $employee->getQualification();
+        $testimonial = $employee->getTestimonial();
+        $bankName = $employee->getBankName();
+        $bankAccountNo = $employee->getBankAccountNo();
+        $postalAddress = $employee->getPostalAddress();
+        $email = $employee->getEmail();
+        $phoneNumber = $employee->getPhoneNumber();
+        $nokName = $employee->getNokName();
+        $nokRelationship = $employee->getNokRelationship();
+        $nokContact = $employee->getNokContact();
+
+        try{
+
+            $sql = "UPDATE employees SET
+                                        pf_no=:pf_no,
+                                        full_name=:full_name,
+                                        job_tile=:job_tile,
+                                        id_no=:id_no, 
+                                        nssf_no=:nssf_no, 
+                                        nhif_no=:nssf_no,
+                                        remuneration=:remuneration,
+                                        job_description=:job_description,
+                                        qualification=:qualification,
+                                        testimonial=:testimonial,
+                                        bank_name=:bank_name,
+                                        bank_account_no=:bank_account_no,
+                                        postal_address=:postal_address,
+                                        email=:email, 
+                                        phone_number=:phone_number,
+                                        nok_name=:nok_name, 
+                                        nok_relationship=:nok_relationship,
+                                        nok_contact=:nok_contact
+                                    WHERE 
+                                        id=:id
+                                    ";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":pf_no", $pfNo);
+            $stmt->bindParam(":full_name",$fullName);
+            $stmt->bindParam(":job_tile", $jobTitle);
+            $stmt->bindParam(":id_no",$idNo);
+            $stmt->bindParam(":nssf_no",$nssfNo);
+            $stmt->bindParam(":nhif_no",$nhifNo);
+            $stmt->bindParam(":remuneration",$remuneration);
+            $stmt->bindParam(":job_description",$jobDescription);
+            $stmt->bindParam(":qualification",$qualification);
+            $stmt->bindParam(":testimonial",$testimonial);
+            $stmt->bindParam(":bank_name",$bankName);
+            $stmt->bindParam(":bank_account_no",$bankAccountNo);
+            $stmt->bindParam(":postal_address",$postalAddress);
+            $stmt->bindParam(":email",$email);
+            $stmt->bindParam(":phone_number",$phoneNumber);
+            $stmt->bindParam(":nok_name",$nokName);
+            $stmt->bindParam(":nok_relationship",$nokRelationship);
+            $stmt->bindParam(":nok_contact",$nokContact);
+            return $stmt->execute() ? true : false;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function delete($id)
