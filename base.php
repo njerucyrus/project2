3220142b6e595a0fb7d07c721942285fe57bcf8a
Samuel Githae <samgithae01@gmail.com>
2017-04-27 07:08:35 +0300

    added right menu

---
+++ base.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: New LAptop
+ * Date: 26/04/2017
+ * Time: 23:55
+ */
+?>
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+<!--start of menu-->
+<?php
+include __DIR__.'/views/right_menu.php';
+?>
+<!--stop of menu-->
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row" id="main" >
+                <div class="col-sm-12 col-md-12 well" id="content">
+                    <?php
+                    include __DIR__.'/views/register_client.php';
+                    ?>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+</div><!-- /#wrapper -->
+<!--javascript-->
+<script src="public/assets/js/jquery-3.2.0.slim.min.js"></script>
+<script src="public/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
--- views/register_group.php
+++ views/register_group.php
@@ -10,15 +10,16 @@ include  __DIR__.'/includes/register_group.inc.php';
 ?>
 <!doctype html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width",initial-scale="1.0">
-    <title> rep sacco</title>
-    <link href= "../public/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../public/assets/css/custom.css " rel="stylesheet">
-    <script src="../public/assets/js/respond.js"></script>
-</head>
+<!--start head-->
+<?php
+include 'head_views.php';
+?>
+<!--stop head-->
 <body>
+
+<?php
+include __DIR__.'/right_menu_views.php';
+?>
 <!-- Name Section -->
 <div class="row">
     <div>
@@ -29,7 +30,12 @@ include  __DIR__.'/includes/register_group.inc.php';
         <?php } ?>
     </div>
 
-    <div class="col-md-8 col-md-offset-1">
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row" id="main" >
+
+            <div class="col-md-8 col-md-offset-1">
         <form class="form-horizontal" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" METHOD="post">
             <fieldset>
 
@@ -108,7 +114,9 @@ include  __DIR__.'/includes/register_group.inc.php';
     </div><!-- /.col-lg-12 -->
 </div><!-- /.row -->
 
-
+        </div>
+    </div>
+</div>
 <!--javascript-->
 <script src="../public/js/jquery-3.2.0.slim.min.js"></script>
 <script src="../public/js/bootstrap.min.js"></script>
