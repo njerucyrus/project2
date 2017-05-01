94cc913f08060754d226904f931939b2d1a82b8a
njerucyrus <njerucyrus5@hotmail.com>
2017-05-01 11:42:24 +0300

    added userinterface

---
+++ src/Hudutech/AppInterface/UserInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudutech
+ * Date: 5/1/17
+ * Time: 11:26 AM
+ */
+
+namespace Hudutech\AppInterface;
+
+
+use Hudutech\Entity\User;
+
+interface UserInterface
+{
+    public function create(User $user);
+    public static function delete($id);
+    public static function destroy();
+    public static function getId($id);
+    public static function all();
+    public function resetPassword($userId, $newPassword);
+    public function changeRole($userId, $role);
+}
